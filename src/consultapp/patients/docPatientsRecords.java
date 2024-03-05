5b64a80b6ced7da0cf64d6bb5ff644fa6f54341a
Pajeto <tolentinopajeto@gmail.com>
2024-03-05 15:59:18 +0800

    new frontend paul renaming

--- src/consultapp/patients/docPatientsRecords.java
+++ src/consultapp/patients/docPatientsRecords.java
@@ -8,12 +8,12 @@ package consultapp.patients;
  *
  * @author User
  */
-public class patients extends javax.swing.JFrame {
+public class docPatientsRecords extends javax.swing.JFrame {
 
     /**
      * Creates new form patients
      */
-    public patients() {
+    public docPatientsRecords() {
         initComponents();
     }
 
@@ -191,20 +191,23 @@ public class patients extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(patients.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(docPatientsRecords.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(patients.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(docPatientsRecords.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(patients.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(docPatientsRecords.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(patients.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(docPatientsRecords.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new patients().setVisible(true);
+                new docPatientsRecords().setVisible(true);
             }
         });
     }
--- src/consultapp/patients/patientsRecords.java
+++ src/consultapp/patients/patientsRecords.java
@@ -8,12 +8,12 @@ package consultapp.patients;
  *
  * @author User
  */
-public class patients2Archived extends javax.swing.JFrame {
+public class patientsRecords extends javax.swing.JFrame {
 
     /**
      * Creates new form patients
      */
-    public patients2Archived() {
+    public patientsRecords() {
         initComponents();
     }
 
@@ -284,14 +284,18 @@ public class patients2Archived extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(patients2Archived.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(patientsRecords.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(patients2Archived.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(patientsRecords.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(patients2Archived.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(patientsRecords.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(patients2Archived.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(patientsRecords.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
         //</editor-fold>
         //</editor-fold>
         //</editor-fold>
@@ -300,7 +304,7 @@ public class patients2Archived extends javax.swing.JFrame {
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new patients2Archived().setVisible(true);
+                new patientsRecords().setVisible(true);
             }
         });
     }
